89f3c997da7f6b2e83260898bb425c5e79fe7832
Fraser Rackham <frackham@googlemail.com>
2014-08-28 18:21:17 +0100

    Added debug console.logs for tests.js

--- src/tests/tests.js
+++ src/tests/tests.js
@@ -1,7 +1,42 @@
-alert('demo - message from test.js');
+//alert('demo - message from test.js');
+
+/* Config
+  
+  We don't autostart tests, as this is triggered by displaying the Debug panel.
+
+*/
+QUnit.config.autostart = false;
+QUnit.begin(function(){
+if (window.console) console.log('Beginning QUnit Tests.'); //Callback fired when QUnit begins. Useful for tracing whether QUnit is working...
+});
+
+QUnit.done(function(){
+if (window.console) console.log('Finished QUnit Tests.'); //Callback fired when QUnit ends. Useful for tracing whether QUnit is working...
+});
+//END CONFIG.
+
+
+//Tests
 QUnit.test( "hello test", function( assert ) {
 
   assert.ok( 1 == "1", "Passed!" );
 
 });
 
+
+QUnit.test( "hello test2", function( assert ) {
+
+  assert.ok( 1 == "2", "Passed!" );
+
+});
+
+
+QUnit.test( "hello test3", function( assert ) {
+
+  assert.ok( 1 == "1", "Passed!" );
+
+});
+
+
+
+QUnit.start();
